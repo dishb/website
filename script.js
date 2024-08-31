2251dbb26094ba8a6be916cfe933aa6613a15f46
dishb <code.dishb@gmail.com>
2024-08-31 16:07:38 -0700

    feat: add theme toggle, scroll up buttons

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 // Select all of the elements needed in the script
 
-// const themeToggle = document.querySelector('#theme-toggle');
-// const themeToggleIcon = document.querySelector('#theme-toggle > i');
+const themeToggle = document.querySelector('#theme-toggle');
+const themeToggleIcon = document.querySelector('#theme-toggle > i');
 const hero = document.querySelector('#hero');
 const scrollDown = document.querySelector('#scroll-down');
+const scrollUp = document.querySelector('#scroll-up');
 const content = document.querySelector('#content');
 const links = document.querySelectorAll('a');
 
 // Dark mode or light mode toggle functionality
 
-// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     document.body.classList.add('dark-mode');
-//     document.body.classList.remove('light-mode');
-// }
+if (window.innerWidth >= 768) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    }
+}
 
-// themeToggle.addEventListener('click', function () {
-//     document.body.classList.toggle('dark-mode');
-//     document.body.classList.toggle('light-mode');
-//     themeToggleIcon.classList.toggle('fa-sun');
-//     themeToggleIcon.classList.toggle('fa-moon');
-// })
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+    themeToggleIcon.classList.toggle('fa-sun');
+    themeToggleIcon.classList.toggle('fa-moon');
+})
 
 // Scroll down to content functionality
 
-scrollDown.addEventListener('click', function () {
+scrollDown.addEventListener('click', () => {
     content.scrollIntoView();
+})
+
+// Scroll up to hero functionality
+
+scrollUp.addEventListener('click', () => {
+    hero.scrollIntoView();
 })
 
 // Add some common attributes to every link element
